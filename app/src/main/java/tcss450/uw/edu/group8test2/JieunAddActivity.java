77bd065915e3a010b7221c9573e197b5b618f09c
Jieun Lee <eunitown@gmail.com>
2017-01-17 08:43:40 -0800

    Add comment

--- app/src/main/java/tcss450/uw/edu/group8test2/JieunAddActivity.java
+++ app/src/main/java/tcss450/uw/edu/group8test2/JieunAddActivity.java
@@ -11,5 +11,7 @@ public class JieunAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jieun_add);
+
+
     }
 }
